b7c9d4d379ae90e88d16104d944d615d34271b1c
Matthew Johnson <matthewwmjohnson@gmail.com>
2024-01-06 14:56:10 -0700

    add root, registrations, and users#show routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
+  root "welcome#index"
+  resources :contact_form, only: %i[new create]
+  # resources :users, only: %i[new create]
+  get "profile", to: "users#show"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
 end
