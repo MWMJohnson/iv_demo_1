f9f0e1d9c143fcfee5a245fe49addd20fb90309a
Matthew Johnson <matthewwmjohnson@gmail.com>
2024-01-06 14:47:58 -0700

    add registrations controller with #new, create, and private params.

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,22 @@
+class RegistrationsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Successfully created account"
+      redirect_to profile_path(@user)
+    else
+      # render :new
+      flash.now[:danger] = @user.errors.full_messages.uniq.to_sentence
+      render :new, status: 422
+    end
+  end
+  
+  private 
+  def user_params 
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+end
