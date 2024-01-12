548f3389f85bfdb7a4af6e2112047e479a9197a6
Matthew Johnson <matthewwmjohnson@gmail.com>
2024-01-12 12:21:35 -0700

    feature data sanitization to user validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[a-zA-Z0-9]+\.)+[a-zA-Z]{2,6})\z/
+  # VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[a-zA-Z0-9]+\.)+[a-zA-Z]{2,6})\z/
+  before_save :sanitize_email
   
-  before_save { email.downcase! }
+  # normalizes :email, with: ->(email) { email.strip.downcase }
+  
   
   validates :first_name, presence: true, length: {maximum: 50 }
   validates :last_name, presence: true, length: {maximum: 50 }
-
+  
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[a-zA-Z0-9]+\.)+[a-zA-Z]{2,6})\z/
   validates :email, format: { with: VALID_EMAIL_REGEX, message: "must be a valid email address"},
   uniqueness: { case_sensitive: false },
   length: { minimum: 4, maximum: 255 },
@@ -15,6 +18,18 @@ class User < ApplicationRecord
   validates :password, presence: true, length: {minimum: 8 }
 
   has_secure_password
+  private
+    # require 'pry'; binding.pry
+
+    #WORKING
+    def sanitize_email
+      write_attribute(:email, email.strip.downcase)
+    end
+
+    # EXPERIMENT
+    # def sanitize_email
+    #   self.email = email.strip.downcase
+    # end
 end
 
 
