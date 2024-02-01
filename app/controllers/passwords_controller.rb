86db02361e273435232dad086413e3e172fd2ce2
Matthew Johnson <matthewwmjohnson@gmail.com>
2024-02-01 14:21:02 -0700

    add change password feature.

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,30 @@
+class PasswordsController < ApplicationController
+  before_action :authenticate_user!
+
+  def edit
+
+  end
+
+  def update
+    if current_user.authenticate(password: params[:password_challenge])
+      if current_user.update(password_params)
+        flash[:notice] = "Your password has been updated successfully."
+        redirect_to dashboard_path(current_user)
+      else
+        flash.now[:alert] = current_user.errors.full_messages.uniq.to_sentence
+        render :edit, status: :unprocessable_entity
+      end
+    else
+      flash.now[:alert] = "Please enter correct current password."
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(
+      :password,
+      :password_confirmation)
+  end
+end
