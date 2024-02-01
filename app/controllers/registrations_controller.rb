413179b155ea0cb50788857038f5ba54b1c415be
Matthew Johnson <matthewwmjohnson@gmail.com>
2024-02-01 14:23:47 -0700

    add notification mailers for user sign up.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -20,6 +20,9 @@ class RegistrationsController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login @user
+      NotifierMailer.send_signup_email(@user.email, @user.first_name).deliver_later
+      user_name = "#{@user.first_name}"+" "+"#{@user.last_name}"
+      NotifierMailer.sign_up_notification(@user.email, user_name).deliver_later
       flash[:notice] = "Successfully created account and logged in"
       redirect_to dashboard_path(@user)
     else
