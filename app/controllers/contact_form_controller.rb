767e2d0a77cbe1d0dce3cc107350e5ee5c2e315c
Matthew Johnson <matthewwmjohnson@gmail.com>
2024-02-01 14:15:15 -0700

    add mailers for contact form message and user receipt.

--- app/controllers/contact_form_controller.rb
+++ app/controllers/contact_form_controller.rb
@@ -4,7 +4,8 @@ class ContactFormController < ApplicationController
     @email = params[:contact_form][:email]
     @message = params[:contact_form][:message]
 
-    NotifierMailer.simple_message(@name, @email, @message).deliver_now
+    NotifierMailer.simple_message(@name, @email, @message).deliver_later
+    NotifierMailer.contact_form_receipt(@name, @email).deliver_later
     flash[:notice] = "Your message has been sent successfully."
     redirect_to :new_contact_form
   end
