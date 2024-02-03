f02706b841355a11c93bdb9f6379e9e1d61b0b90
Matthew Johnson <matthewwmjohnson@gmail.com>
2024-02-03 13:30:59 -0700

    edit: insight typos in notifier mailer.

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -5,15 +5,15 @@ class NotifierMailer < ApplicationMailer
   def simple_message(name, email, message)
     mail(
       "reply-to": email_address_with_name(email, "#{name}"),
-      subject: "Insight Valuations New Inquiry from #{name}",
+      subject: "Insight Valuations -  New Inquiry from #{name}",
       body: message
     )
   end
   def sign_up_notification(email, name)
     mail(
       "reply-to": email_address_with_name(email, "#{name}"),
-      subject: "Insight Valuations New user has registered, #{name}",
-      body: "Hello Justin, \n\n Congrats a new user, #{name}, has registered at your website.\n\nBest regards,\nInsightful Valuations LLC"
+      subject: "Insight Valuations -  New user has registered, #{name}",
+      body: "Hello Justin, \n\n Congrats a new user, #{name}, has registered at your website.\n\nBest regards,\nInsight Valuations LLC"
     )
   end
 
@@ -28,16 +28,16 @@ class NotifierMailer < ApplicationMailer
   def send_signup_email(email, name)
     mail(
       to: email,
-      subject: "Insightful Valuations LLC - Thank you #{name}, you have successfully created account!",
-      body: "Hello #{name},\n\nThank you for signing up, your account has been successfully created. We hope you enjoy our website and always welcome your feedback!\n\nBest regards,\nInsightful Valuations LLC"
+      subject: "Insight Valuations LLC - Thank you #{name}, you have successfully created account!",
+      body: "Hello #{name},\n\nThank you for signing up, your account has been successfully created. We hope you enjoy our website and always welcome your feedback!\n\nBest regards,\nInsight Valuations LLC"
     )
   end
 
   def contact_form_receipt(name, email)
     mail(
       to: email,
-      subject: "Insightful Valuations LLC - Thank you #{name} for your message!",
-      body: "Hello #{name},\n\n We are glad to hear from you! We strive to reply to all messages received in 24-48 hours. In case you do not hear back from us within two business days, please call us at (346) 554-3706.\n\nBest regards,\nInsightful Valuations LLC"
+      subject: "Insight Valuations LLC - Thank you #{name} for your message!",
+      body: "Hello #{name},\n\n We are glad to hear from you! We strive to reply to all messages received in 24-48 hours. In case you do not hear back from us within two business days, please call us at (346) 554-3706.\n\nBest regards,\nInsight Valuations LLC"
     )
   end
   
